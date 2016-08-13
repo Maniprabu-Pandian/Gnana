9cddfea90af194be7127f850a911ad7737adb94d
Maniprabu-Pandian <maniprabhulive.22@gmail.com>
2016-08-13 12:35:11 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+
+#include<string.h>
+
+
+int main()
+
+{
+  
+  char *a=(char*)malloc(sizeof(char));
+
+    scanf("%s",a);
+
+    int l,i,j;
+
+    l=strlen(a);
+
+    if(l%2==0)
+
+    {
+ 
+       for(i=0;i<l;i++)
+
+        {
+  
+          for(j=0;j<l;j++)
+ 
+           {
+  
+              if(i==j || j==l-i-1)
+
+                printf("%c",a[j]);
+ 
+               else
+ 
+               printf(" ");
+  
+          }
+     
+       printf("\n");
+  
+      }
+   
+ }
+   
+ return 0;
+
+}
